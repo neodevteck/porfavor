701131a7fa8a32d4505ef1be1dd01fa99c2e0b48
carlosgomez247 <carlosgomez.x247@gmail.com>
2022-05-09 21:31:42 -0500

    soem

--- src/query-hooks/useActividades.js
+++ src/query-hooks/useActividades.js
@@ -1,14 +1,28 @@
-import { useQuery } from 'react-query'
-import axios from 'axios'
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
-export const fetchActividades = (vFechaInicial,vFechaFinal,vArrIds,vOpcionVista) =>   
-     axios
-     .post('frmCalendarioV2.aspx/ObtenerActividadesxTerceIdxFechaInixFechaFin', {FechaInicial:vFechaInicial,FechaFinal:vFechaFinal,ArrIds:vArrIds,Tipo:vOpcionVista}, {
-       headers: { 'Content-Type': 'application/json' }
-     })
+export const fetchActividades = (
+  FechaInicial,
+  FechaFinal,
+  ArrIds,
+  OpcionVista
+) =>
+  axios
+    .post(
+      'frmCalendarioV2.aspx/ObtenerActividadesxTerceIdxFechaInixFechaFin',
+      {
+        FechaInicial: FechaInicial,
+        FechaFinal: FechaFinal,
+        ArrIds: ArrIds,
+        Tipo: OpcionVista,
+      },
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
     // .get('http://localhost:3003/actividades')
-    .then((res) => res.data)
+    .then((res) => res.data);
 
 export default function useActividades() {
-  return useQuery(['actividades'], fetchActividades)
+  return useQuery(['actividades'], fetchActividades);
 }
